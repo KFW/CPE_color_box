65824090a88cd0bf702e27e12ab53d7fdbc673be
Keith F. Woeltje <comm@woeltje.org>
2022-08-21 19:46:16 -0500

    control color of neopixels on CPE with rheostats

--- code.py
+++ code.py
@@ -1,3 +1,4 @@
+#control color of neopixels on Circuit Playground Express with rheostats.
 import time
 import board
 import analogio
@@ -5,23 +6,23 @@ import neopixel
 
 MAXREAD = 65520  # this is max reading on ADC
 
-rpin = analogio.AnalogIn(board.A7)
-gpin = analogio.AnalogIn(board.A6)
-bpin = analogio.AnalogIn(board.A2)
+rpin = analogio.AnalogIn(board.A3)
+gpin = analogio.AnalogIn(board.A4)
+bpin = analogio.AnalogIn(board.A7)
 pixels = neopixel.NeoPixel(board.NEOPIXEL, 10, brightness=1, auto_write=False)
 
 def getValues(rpin, gpin, bpin):  # helper
     r_raw = rpin.value
     g_raw = gpin.value
     b_raw = bpin.value
-    rval = (r_raw * 255 / MAXREAD)  # returns value between 0 and 255
-    gval = (g_raw * 255 / MAXREAD)  # returns value between 0 and 255
-    bval = (b_raw * 255 / MAXREAD)  # returns value between 0 and 255
+    rval = int(r_raw * 255 / MAXREAD)  # returns value between 0 and 255
+    gval = int(g_raw * 255 / MAXREAD)  # returns value between 0 and 255
+    bval = int(b_raw * 255 / MAXREAD)  # returns value between 0 and 255
     return (rval, gval, bval)
 
 while True:
     R, G, B = getValues(rpin, gpin, bpin)
-    # print(R, G, B)
+    print(R, G, B)
     pixels.fill((R,G,B))
     pixels.show()
     time.sleep(0.1)
